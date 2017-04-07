fd9aa0cb4f1cff50cc2313f04556aa05c0c12587
Erik <erik.langenborg@gmail.com>
2017-04-07 02:01:44 -0400

    Word move distance calculation

--- asg/tests/tests_word2vec.py
+++ asg/tests/tests_word2vec.py
@@ -2,10 +2,10 @@
 
 import unittest
 
-from asg.word2vec import vec
+from asg.word2vec import vec, similarity, tokenize, word_mover_distance
 
 
-class TestEmbeddings(unittest.TestCase):
+class TestBasic(unittest.TestCase):
     """Test the embedding tools. This requires the main data file and will take a long time."""
 
     def test_basic_embedding(self):
@@ -13,6 +13,25 @@ class TestEmbeddings(unittest.TestCase):
         result = vec("king")
         self.assertEqual(len(result), 300)
         self.assertAlmostEqual(result[0], 0.12597656, 5)
+
+    def test_basic_similarity(self):
+        """Test similarity between words"""
+        result = similarity("king", "queen")
+        self.assertAlmostEqual(result, 0.651095683538665, 5)
+
+    def test_basic_tokenize(self):
+        """Test tokenize of sentence"""
+        result = tokenize(
+            "The quick brown fox jumped over the lazy dog?")
+        self.assertListEqual(
+            result, ['quick', 'brown', 'fox', 'jumped', 'lazy', 'dog'])
+
+    def test_basic_wmd(self):
+        """Test word move distance"""
+        result = word_mover_distance(
+            "Obama speaks to the media in Illinois.",
+            "The President greets the press in Chicago.")
+        self.assertAlmostEqual(result, 0.042881972611898585, 5)
 
 
 if __name__ == '__main__':
--- asg/word2vec.py
+++ asg/word2vec.py
@@ -2,7 +2,15 @@ import os
 
 from gensim.models import Word2Vec
 from gensim import models
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
 from .datadirectory import data_directory
+
+
+_tokenizer = RegexpTokenizer(r'\w+')
+_stopset = set(stopwords.words('english'))
 
 
 _mode_name = os.path.join(
@@ -19,3 +27,35 @@ def vec(word):
 def similarity(word_01, word_02):
     """Similarity between two words"""
     return _embedding_model.similarity(word_01, word_02)
+
+
+def tokenize(text):
+    """Reduce a string to non-stopword tokens"""
+    return [word for word in _tokenizer.tokenize(text.lower()) if word not in _stopset]
+
+
+def sentence_embedding(text):
+    """Convert a sentence to text embedding"""
+    return [vec(word) for word in _tokenizer.tokenize(text)]
+
+
+def _find_min_distance(word_source, tokens_target):
+    """Find the minimum similarity between source word and all tokens"""
+    return min([similarity(word_target, word_source) for word_target in tokens_target])
+
+
+def word_mover_distance(text_source, text_target):
+    """
+    Measures the distance from the source text to the target text
+
+    Minimum cumulative distance that all words in document 1 need to travel to exactly match document 2.
+
+    http://jmlr.org/proceedings/papers/v37/kusnerb15.pdf
+    """
+    tokens_source = tokenize(text_source)
+    tokens_target = tokenize(text_target)
+
+    scores = [_find_min_distance(token, tokens_target)
+              for token in tokens_source]
+
+    return sum(scores) / len(tokens_source)
