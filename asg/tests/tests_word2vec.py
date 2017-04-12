2b582a2763e6aab2d26aea484af460c51377a3e2
Erik Langenborg <erik.langenborg@gmail.com>
2017-04-12 00:45:39 -0400

    Update tests for new design

--- asg/tests/tests_word2vec.py
+++ asg/tests/tests_word2vec.py
@@ -2,33 +2,34 @@
 
 import unittest
 
-from asg.word2vec import vec, similarity, tokenize, word_mover_distance
+from asg.word2vec import Word2Vec
 
 
 class TestBasic(unittest.TestCase):
     """Test the embedding tools. This requires the main data file and will take a long time."""
+    _word2vec = Word2Vec()
 
     def test_basic_embedding(self):
         """Read a common embedding"""
-        result = vec("king")
+        result = TestBasic._word2vec.vec("king")
         self.assertEqual(len(result), 300)
         self.assertAlmostEqual(result[0], 0.12597656, 5)
 
     def test_basic_similarity(self):
         """Test similarity between words"""
-        result = similarity("king", "queen")
+        result = TestBasic._word2vec.similarity("king", "queen")
         self.assertAlmostEqual(result, 0.651095683538665, 5)
 
     def test_basic_tokenize(self):
         """Test tokenize of sentence"""
-        result = tokenize(
+        result = TestBasic._word2vec.tokenize(
             "The quick brown fox jumped over the lazy dog?")
         self.assertListEqual(
             result, ['quick', 'brown', 'fox', 'jumped', 'lazy', 'dog'])
 
     def test_basic_wmd(self):
         """Test word move distance"""
-        result = word_mover_distance(
+        result = TestBasic._word2vec.word_mover_distance(
             "Obama speaks to the media in Illinois.",
             "The President greets the press in Chicago.")
         self.assertAlmostEqual(result, 0.042881972611898585, 5)
