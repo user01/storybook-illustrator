94784fea0cb35f15a3f669e46d8521d15e705516
Erik Langenborg <erik.langenborg@gmail.com>
2017-04-12 00:41:57 -0400

    Extracted basic operations

--- asg/labels.py
+++ asg/labels.py
@@ -7,41 +7,68 @@ import json
 from .datadirectory import data_directory
 
 
-_json_subsets = ['test', 'train', 'val']
-_json_groups = [('dii', 'description-in-isolation'),
-                ('sis', 'story-in-sequence')]
+class Annotations:
+    
+    _json_subsets = ['test', 'train', 'val']
+    _json_groups = [('dii', 'description-in-isolation'),
+                    ('sis', 'story-in-sequence')]
+    _labels = None
 
-_label_data = {}
-for directory, label in _json_groups:
-    _label_data[directory] = {}
-    for subset in _json_subsets:
-        path = os.path.join(data_directory, directory,
-                            '{}.{}.json'.format(subset, label))
-        with open(path) as data_file:
-            _label_data[directory][subset] = json.load(data_file)
+    _annotations_train = None
+    _annotations_test = None
 
+    @staticmethod
+    def _label_data():
+        if Annotations._labels is not None:
+            return Annotations._labels
 
-def _annotation_to_dict(label_data, subset, group):
-    """
-    Reduce an annotation to only the relevant data
-    """
-    annotations_ids = [a[0]["photo_flickr_id"] for a in label_data[group][subset]['annotations']]
-    annotations_texts = [a[0]["text"] for a in label_data[group][subset]['annotations']]
-    return dict(zip(annotations_ids, annotations_texts))
+        Annotations._labels = {}
+        for directory, label in Annotations._json_groups:
+            Annotations._labels[directory] = {}
+            for subset in Annotations._json_subsets:
+                path = os.path.join(data_directory, directory,
+                                    '{}.{}.json'.format(subset, label))
+                with open(path) as data_file:
+                    Annotations._labels[directory][subset] = json.load(
+                        data_file)
 
+        return Annotations._labels
 
-def _annotations(label_data, subset):
-    """
-    Gather annotation into dictionary
+    @staticmethod
+    def _annotation_to_dict(label_data, subset, group):
+        """
+        Reduce an annotation to only the relevant data
+        """
+        annotations_ids = [a[0]["photo_flickr_id"]
+                           for a in label_data[group][subset]['annotations']]
+        annotations_texts = [a[0]["text"]
+                             for a in label_data[group][subset]['annotations']]
+        return dict(zip(annotations_ids, annotations_texts))
 
-    key - string - value that matches [image filename].jpg
-    value - string of santanized text description
-    """
+    @staticmethod
+    def _annotations(label_data, subset):
+        """
+        Gather annotation into dictionary
 
-    annotations_dii = _annotation_to_dict(label_data, subset, "dii")
-    annotations_sis = _annotation_to_dict(label_data, subset, "sis")
-    return {**annotations_sis, **annotations_sis}
+        key - string - value that matches [image filename].jpg
+        value - string of sanitized text description
+        """
 
+        annotations_dii = Annotations._annotation_to_dict(label_data, subset, "dii")
+        annotations_sis = Annotations._annotation_to_dict(label_data, subset, "sis")
+        return {**annotations_sis, **annotations_sis}
 
-annotations_train = _annotations(_label_data, 'train')
-annotations_test = _annotations(_label_data, 'test')
+    @staticmethod
+    def annotations_train():
+        """Returns the training annotations dictionary"""
+        if Annotations._annotations_train is None:
+            Annotations._annotations_train = Annotations._annotations(Annotations._label_data(), 'train')
+        return Annotations._annotations_train
+
+    @staticmethod
+    def annotations_test():
+        """Returns the testing annotations dictionary"""
+        if Annotations._annotations_test is None:
+            Annotations._annotations_test = Annotations._annotations(Annotations._label_data(), 'test')
+        return Annotations._annotations_test
+
